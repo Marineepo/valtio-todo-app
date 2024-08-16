b27b21f3fc870469e3af60076c8a1d6c0f226348
Benjamin Nicklaus <marineepo@gmail.com>
2024-08-16 16:12:28 -0400

    created the store.ts and the starting of the App file

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store.ts
@@ -0,0 +1,42 @@
+import { proxy } from 'valtio';
+
+// To understand the code below and how it works, please reference to the store.txt file in the same directory.
+// There is a break down of each function and how it works.
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodoStore {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
+
+const todoStore = proxy<TodoStore>({
+  todos: [],
+  
+  addTodo: (text: string) => {
+    todoStore.todos.push({
+      id: Date.now(),
+      text,
+      completed: false,
+    });
+  },
+  
+  toggleTodo: (id: number) => {
+    const todo = todoStore.todos.find(todo => todo.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+    }
+  },
+  
+  removeTodo: (id: number) => {
+    todoStore.todos = todoStore.todos.filter(todo => todo.id !== id);
+  }
+});
+
+export default todoStore;
