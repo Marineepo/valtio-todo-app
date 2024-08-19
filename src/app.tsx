9ec0139783ad22ba89acc72d750a898cef3a7e56
Benjamin Nicklaus <marineepo@gmail.com>
2024-08-19 09:24:20 -0400

    added in the App file implementation

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useSnapshot } from 'valtio';
+import todoStore from './store';
 
 const App: React.FC = () => {
+  const snapshot = useSnapshot(todoStore);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      todoStore.addTodo(newTodo);
+      setNewTodo('');
+    }
+  };
 
   return (
     <div>
       <h1>To-Do List</h1>
+      <input 
+        type="text" 
+        value={newTodo} 
+        onChange={(e) => setNewTodo(e.target.value)} 
+        placeholder="Add a new to-do"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      
+      <ul>
+        {snapshot.todos.map((todo) => (
+          <li key={todo.id}>
+            <input 
+              type="checkbox" 
+              checked={todo.completed} 
+              onChange={() => todoStore.toggleTodo(todo.id)} 
+            />
+            {todo.text}
+            <button onClick={() => todoStore.removeTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
